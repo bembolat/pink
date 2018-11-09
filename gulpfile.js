f822c3193e12913da00b02a8176068ce1900cc16
bembolat <barkadwood@gmail.com>
2018-11-09 19:38:41 +0300

    добавил изменения в gulpfile.js и packaje

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+
+
+gulp.task("style", function() {
+  gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"],function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{sass,scss}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+})
